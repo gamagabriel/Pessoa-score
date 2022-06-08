27ac9ae861cd45a17d4de302b1de2c423260ec7e
Gabriel Gama <58527214+gamagabriel@users.noreply.github.com>
2022-06-08 20:37:39 -0300

    Initial setup

---
+++ src/test/java/com/pessoa/score/PessoaComScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pessoa.score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaComScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
